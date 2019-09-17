93c6c8e5e99383038372128246e12b7e81d898bd
Matt Crowder <mcrowder65@gmail.com>
2019-09-17 17:56:18 -0400

    Created switch, need to link it now

--- src/slides/wrong/__tests__/switch.test.js
+++ src/slides/wrong/__tests__/switch.test.js
@@ -2,16 +2,19 @@ import React from "react";
 import { shallow } from "enzyme";
 import Switch from "../switch";
 
-describe("getChecked", () => {
-  [true, false].forEach(checked => {
-    test(`When passing checked as ${checked}, it returns ${checked}`, () => {
-      const checkedIsTrue = shallow(<Switch checked={checked} />);
-      expect(checkedIsTrue.instance().getChecked()).toEqual(checked);
-    });
+describe("getChecked()", () => {
+  test(`When passing checked, it returns checked`, () => {
+    let checked = true;
+    let component = shallow(<Switch checked={checked} />);
+    expect(component.instance().getChecked()).toEqual(checked);
+
+    checked = false;
+    component = shallow(<Switch checked={checked} />);
+    expect(component.instance().getChecked()).toEqual(checked);
   });
 });
 
-describe("toggle", () => {
+describe("toggle()", () => {
   test("when passing toggle as a prop, it uses it", () => {
     const toggle = jest.fn();
     const component = shallow(<Switch toggle={toggle} />);
--- src/slides/wrong/wrong.js
+++ src/slides/wrong/wrong.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Switch from "./switch";
 import { Container } from "../../reusable/container";
+import { Text } from "../../reusable/text";
 
 const Wrong = () => {
   const [checked, setChecked] = React.useState(false);
   return (
     <Container>
+      <Text>Controlled</Text>
       <Switch checked={checked} toggle={() => setChecked(!checked)} />
       <Switch checked={checked} toggle={() => setChecked(!checked)} />
+      <Text>Uncontrolled</Text>
       <Switch />
     </Container>
   );
